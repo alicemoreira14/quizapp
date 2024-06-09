3975f9ffef8e7d2b57a9901401c8184db1a28093
alice moreira <alicinha.moreira14@gmail.com>
2024-06-09 22:34:02 +0000

    Criação de u helper responsável por mudar o tema

--- helpers/tema-helper.js
+++ helpers/tema-helper.js
@@ -1,15 +1,6 @@
-import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+let tema = "claro"
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-const botaoTema = document.querySelector(".tema button")
-const body = document.querySelector("body")
-
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
-})
-
-function verificarTema(body, botaoTema) {
+export function trocarTema(body, botaoTema) {
     if (localStorage.getItem("tema")){
         tema = localStorage.getItem("tema")
     }
@@ -17,21 +8,22 @@ function verificarTema(body, botaoTema) {
     if (tema === "claro") {
         body.classList.add("escuro")
         localStorage.setItem("tema", "escuro")
-
+        botaoTema.style.justifyContent = "flex-end"
     } else {
         body.classList.remove("escuro")
         localStorage.setItem("tema", "claro" )
+        botaoTema.style.justifyContent = "flex-start"
+
     }
 }
 
-function verificarTema() {
+export function verificarTema(body, botaoTema) {
     if (localStorage.getItem("tema")) {
         tema= localStorage.getItem("tema")
     }
 
     if (tema === "escuro") {
         body.classList.add("escuro")
+        botaoTema.style.justifyContent = "flex-end"
     }
 }
-
-verificarTema()
